cf1e2d1e13f8bc73aa699c4d3b01b34eb90705c4
stigakl <stigakl@outlook.com>
2021-09-29 00:37:35 +0200

    add spinner on loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { ThemeProvider } from 'styled-components'; 
 import { Theme } from './Components/Themes/themes'; 
 import { Nav } from './Components/NavBar/Nav' 
-import { useEffect, useState } from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import { Jokes, Auth } from './api/agent'; 
-import { Container } from 'semantic-ui-react'; 
+import { Container, Loader, Segment, Dimmer } from 'semantic-ui-react'; 
 import { DashBoard } from './Components/DashBoard';
 import styled from 'styled-components'; 
 import { isAuth, setToken } from './utils/auth';
@@ -23,6 +23,11 @@ const StyledContainer = styled.div`
   margin-top: 15rem; 
 `;
 
+const StyledLoader = styled(Loader)`
+    &&& {
+      margin-top: 20rem;
+    }
+`;
 function App() {
 
   const [jokes, setJokes] = useState([]); 
@@ -70,24 +75,27 @@ function App() {
       setJokes(response);
       setLoading(false);  
     }); 
-    setLoading(false);  
   }, [])
 
   return (
       <ThemeProvider theme={Theme}>
         <Nav />
-        {isAuthenticated && !loading &&
           <Container style={{ marginTop: "7em" }}>
-            <Title>Antall aktive vitser: { jokes.length }</Title>
-            <DashBoard 
-            jokes={jokes} 
-            deleteJoke={handleDeleteJoke}
-            setNewJokeMode={setNewJokeMode}
-            newJokeMode={newJokeMode} 
-            handleNewJoke={handleNewJoke}
-            />
+          {isAuthenticated && !loading &&
+            <React.Fragment>
+              <Title>Antall aktive vitser: { jokes.length }</Title>
+              <DashBoard 
+              jokes={jokes} 
+              deleteJoke={handleDeleteJoke}
+              setNewJokeMode={setNewJokeMode}
+              newJokeMode={newJokeMode} 
+              handleNewJoke={handleNewJoke}
+              />
+            </React.Fragment>
+          }
+
+          {loading && <StyledLoader inline='centered' active/>}
         </Container>
-        }
 
         {!isAuthenticated && !loading &&
           <StyledContainer>
